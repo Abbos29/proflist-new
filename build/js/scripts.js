70803d720bb65563a1f2e5c4d7a3c8d693e7265f
Abbos <abbosvaliev2003@gmail.com>
2023-11-18 14:06:20 +0500

    fix: add burger

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -10,7 +10,7 @@ const body = document.querySelector('body');
 
 
 
-close.forEach(function(closeBtn) {
+close.forEach(function (closeBtn) {
     closeBtn.addEventListener('click', function (e) {
         e.stopPropagation();
         modal1.classList.remove('active');
@@ -22,13 +22,13 @@ close.forEach(function(closeBtn) {
 
 
 modalBtn1.addEventListener('click', function (e) {
-    e.stopPropagation(); 
+    e.stopPropagation();
     modal1.classList.add('active');
     body.classList.add('active');
 });
 
 modalBtn2.addEventListener('click', function (e) {
-    e.stopPropagation(); 
+    e.stopPropagation();
     modal2.classList.add('active');
     body.classList.add('active');
 });
@@ -41,3 +41,62 @@ document.body.addEventListener('click', function (e) {
         body.classList.remove('active');
     }
 });
+
+
+
+
+
+
+
+
+
+
+// Мобильное меню бургер
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active-burger')
+            body.classList.add('locked')
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+        }
+    })
+    menu.addEventListener("click", (event) => {
+        if (event.target) {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+        }
+    })
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+        }
+    })
+}
+burgerMenu()
+
+
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+function fixedNav() {
+    const nav = document.querySelector('nav')
+
+    // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+    const breakpoint = 1
+    if (window.scrollY >= breakpoint) {
+        nav.classList.add('fixed__nav')
+    } else {
+        nav.classList.remove('fixed__nav')
+    }
+}
+window.addEventListener('scroll', fixedNav)
+
